468917fa6ab3ab5b873c2bb7eb91310c0f9015c1
januwA <ajanuw1641845087@sina.com>
2020-01-05 20:01:26 +0800

    优化

--- sketch.js
+++ sketch.js
@@ -11,7 +11,20 @@ function setup() {
   ctx.drop(gotFile); // 获取拖拽上传的图片
   handleFileInput(); // 获取input上传的图片
   gotLoadingImg(); // 获取input中输入的图片
-  handleDownload();
+
+  createButton("Download").mousePressed(e => {
+    if (!img) return null;
+    saveCanvas(ctx, gfile.name);
+    // if (gfile.data.startsWith("http")) {
+    //   return alert("网络图片请手动下载!");
+    // }
+    // const dataURL = ctx.canvas.toDataURL(gfile.file.type);
+    // downloadButton.attribute("download", gfile.name);
+    // downloadButton.attribute("href", dataURL);
+    // setTimeout(() => {
+    //   downloadButton.attribute("href", "javascript: (void 0)");
+    // }, 1000);
+  });
 }
 
 function gotFile(file) {
@@ -24,7 +37,8 @@ function draw() {
     for (let y = 0; y < height; y++) {
       if (y % 2 === 0) {
         stroke(0, 200);
-        line(0, y, width, y);
+        const h = y * 2;
+        line(0, h, width, h);
       }
     }
   } else {
@@ -48,10 +62,9 @@ function handleFileInput() {
 function setImg(f) {
   gfile = f;
   img = createImg(f.data, () => {
-    resizeCanvas(Math.min(img.width, 600), Math.min(img.height, 400));
+    resizeCanvas(Math.max(img.width, 600), Math.max(img.height, 400));
   });
   img.hide();
-  // img.attribute('crossOrigin', 'Anonymous')
 }
 
 function gotLoadingImg() {
@@ -68,24 +81,8 @@ function gotLoadingImg() {
       file: {
         type: `image/${
           /jpg/i.test(r.groups.filetype) ? "jpeg" : r.groups.filetype
-        }`,
+          }`,
       },
     });
-  });
-}
-
-function handleDownload() {
-  downloadButton = createA("javascript: (void 0)", "Download");
-  downloadButton.mouseClicked(e => {
-    if (!img) return null;
-    if (gfile.data.startsWith("http")) {
-      return alert("网络图片请手动下载!");
-    }
-    const dataURL = ctx.canvas.toDataURL(gfile.file.type);
-    downloadButton.attribute("download", gfile.name);
-    downloadButton.attribute("href", dataURL);
-    setTimeout(() => {
-      downloadButton.attribute("href", "javascript: (void 0)");
-    }, 1000);
   });
 }
